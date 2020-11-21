726f12390705c8a75fdaf029564165db00d9e238
christy lo <christylo2000@hotmail.com>
2020-11-21 05:28:14 -0800

    added budget headers to home page

--- components/Home.js
+++ components/Home.js
@@ -3,6 +3,9 @@ import { View, Image, SafeAreaView, Text, StyleSheet } from 'react-native';
 import Feather from 'react-native-vector-icons/Feather';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import recentTransactionsData from '../assets/data/recentTransactionsData';
+//import Inter from '../assets/fonts/Inter-Regular-slnt=0.tff';
+
+//Inter.loadFont();
 
 export default Home = () => {
     return (
@@ -15,10 +18,36 @@ export default Home = () => {
                     </Image>
                 </View>
             </SafeAreaView>
+            {/* budget displays */}
+            <View style={styles.budgetDisplaysWrapper}>
+                <Text style={styles.descriptionTitles}>Allowance This Month</Text>
+                <Text style={styles.descriptionTitles}>Money Remaining</Text>
+            </View>
         </View>
     )
 };
 
 const styles = StyleSheet.create({
-
+    container: {
+        flex: 1,
+    },
+    headerWrapper: {
+        flexDirection: 'row',
+        justifyContent: "space-between",
+        paddingHorizontal: 35,
+        paddingTop: 20
+    },
+    pageHeader: {
+        width: 303,
+        height: 62
+    },
+    budgetDisplaysWrapper: {
+        flexDirection: 'row',
+        justifyContent: "space-between",
+        paddingHorizontal: 20,
+        paddingTop: 20
+    },
+    descriptionTitles: {
+        fontSize: 15,
+    }
 });
