85212959d5ba24802968e745bcf44f13bdf3e569
William <williamzhang8@live.ca>
2020-11-21 16:40:04 -0800

    Kid Flow

---
+++ components/Kidconfirm.js
@@ -0,0 +1,77 @@
+import 'react-native-gesture-handler';
+import React, {useEffect, useState} from 'react';
+import {Button, Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Icon from 'react-native-vector-icons/Ionicons';
+import {Camera} from "expo-camera";
+
+export default Kidconfirm = () => {
+
+    return (
+        <View style={styles.maincontainer}>
+            <View style={styles.subcontainer}>
+                <Text style={{color: '#FFF', fontSize: 18}}> Request </Text>
+                <View style={{paddingTop: 30, paddingBottom:20}}>
+                    <Text style={{color: '#FFF', fontSize: 42}}> Waiting </Text>
+                </View>
+                <Text style={{color: '#FFF', fontSize: 18}}> Request sent. </Text>
+                <Text style={{color: '#FFF', fontSize: 18}}> Waiting for Taylor </Text>
+                <View style={styles.temp1}>
+                    <View>
+                        <Button
+                            title="Cancel"
+                            color="#568259"
+                            onPress={() => takePicture()}
+                        />
+                    </View>
+                </View>
+            </View>
+        </View>
+    )
+
+
+}
+
+
+
+const styles = StyleSheet.create({
+    temp2:{
+        padding: 10,
+    },
+    temp1: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        paddingTop: 20,
+    },
+    maincontainer: {
+        flex: 1,
+        borderRadius: 10,
+        overflow: 'hidden',
+    },
+    subcontainer: {
+        flex: 1,
+        padding: 20,
+        backgroundColor: '#1BB55C65',
+        alignItems: 'center',
+    },
+    headerWrapper: {
+        flexDirection: 'row',
+        justifyContent: "space-between",
+        paddingHorizontal: 35,
+        paddingTop: 20
+    },
+    pageHeader: {
+        width: 303,
+        height: 62
+    },
+    budgetDisplaysWrapper: {
+        flexDirection: 'row',
+        justifyContent: "space-between",
+        paddingHorizontal: 20,
+        paddingTop: 20
+    },
+    descriptionTitles: {
+        fontSize: 15,
+    }
+});
