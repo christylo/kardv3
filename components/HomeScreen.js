11bb481d935b5eafe5a9e3c12fb2f474f9b35444
Zahin <alvii147@gmail.com>
2020-11-21 20:02:50 -0500

    added pie chart

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Button, View, Image, SafeAreaView, Text, StyleSheet } from 'react-native';
+import { VictoryPie } from 'victory-native'
 
 export default HomeScreen = ({ navigation: { navigate } }) => {
     return (
@@ -19,6 +20,16 @@ export default HomeScreen = ({ navigation: { navigate } }) => {
             </View>
             {/* test button */}
             <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                <VictoryPie
+                    data={[
+                        { x: "Cash", y: 35 },
+                        { x: "Snacks", y: 20 },
+                        { x: "Meals", y: 20 },
+                        { x: "Others", y: 55 }
+                    ]}
+                    colorScale={["#f1fffa", "#96e6b3", "#1bb55c", "#568259"]}
+                    height={300}
+                />
                 <Text>This is the home screen of the app</Text>
                 <Button
                     onPress={() =>
